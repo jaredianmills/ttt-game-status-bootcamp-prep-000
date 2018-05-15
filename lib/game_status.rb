c52a8e66e4ff7486d378ec3f20e5dab6332c7c29
jaredianmills <jaredianmills@gmail.com>
2018-05-15 16:51:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -57,8 +57,8 @@ def winner(board)
       return "X"
     elsif board[index] == "O"
       return "O"
-    else
-      winning_player = false
+    else 
+      winning_player = nil
     end
   end
   winning_player
