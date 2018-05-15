bc4c6265bd3fb063eec8c754a2362ef2a3a30463
jaredianmills <jaredianmills@gmail.com>
2018-05-15 16:20:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,7 +29,7 @@ def won?(board)
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     else
-      false
+      return false
     end
   end
 end
@@ -39,4 +39,3 @@ end
 
 def draw?
 end
-
